a3f9bbe1a9168858b1c97eef3dfe4e7c24c6721e
sekhabiruddin <sksabiruddin81@gmail.com>
2024-09-15 20:04:35 +0530

    add localhost

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ const bodyParser = require("body-parser");
 const errorMiddleware = require("./middleware/error");
 const newsRoutes = require("./controller/news");
 const cors = require("cors");
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.use(errorMiddleware);
 app.use(
   cors({
-    origin: ["https://aco-news-frontend.vercel.app"],
+    origin: ["https://aco-news-frontend.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
-app.use("/api/news", newsRoutes);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/news", newsRoutes); // Place routes after CORS
+
+app.use(errorMiddleware); // Place error middleware last
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
